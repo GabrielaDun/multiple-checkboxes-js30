76685e154baa9ef8b5598a1fd2b09001cf02ba3c
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-10-06 22:15:26 +0200

    Build structure for the home page

--- parkings/src/components/pages/Home/Home.js
+++ parkings/src/components/pages/Home/Home.js
@@ -1,11 +1,40 @@
-import { Container } from 'reactstrap';
+import Button from '../../common/Button/Button';
+import PageTitle from '../../common/PageTitle/PageTitle';
+import styles from './Home.module.scss'
 
 
 const Home= () => (
   <div>
-    <Container>
-        <h1>HOME</h1>
-    </Container>
+    <div className={styles.container}>
+        <h1>GET YOUR ONLINE PARKING TICKETS</h1>
+        <div className={styles.main}>
+            <div className={styles.content}>
+                <div className={styles.carChoice}>
+                    <PageTitle>Choose your vehicule</PageTitle>
+                </div>
+                <div className={styles.hoursChoice}>
+                    <PageTitle>Number of hours:</PageTitle>
+                </div>
+                <div className={styles.location}>
+                    <PageTitle>Parking location</PageTitle>
+                    <h3>Red area</h3>
+                </div>
+                <div className={styles.result}>
+                    <PageTitle>Your fee per Xh:</PageTitle>
+                    <p>On this ride you could have saved:</p>
+                    <p>1.3 euro by using ecological car</p>
+                    <p>1.5 euro by parking in a green area</p>
+                </div>
+                <Button>Buy the ticket</Button>
+            </div>
+            <div className={styles.photo}>
+            <img
+                className={styles.image}
+                alt="Map"
+                src={`${process.env.PUBLIC_URL}/images/map.jpg`} />
+            </div>
+        </div>
+    </div>
   </div>
 );
 
